d587b11dae75fefe022527837fb7688e9c0773b4
Nacho <nachogamallolafon123@gmail.com>
2025-03-11 19:34:36 +0100

    Primera parte de ejercicios de Teoría.

---
+++ PFuncional/src/main/java/org/example/ExPilas/TeoríaColecciones/TeoriaEx.java
@@ -0,0 +1,90 @@
+
+package org.example.ExPilas.TeoríaColecciones;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class TeoriaEx {
+
+    static Scanner entry = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //Ex1();
+        //Ex2();
+        Ex3();
+    }
+
+    public static void Ex1 (){
+
+        Stack<String> pila = new Stack<>();
+        pila.push("H");
+        pila.push("O");
+        pila.push("L");
+        pila.push("A");
+        pila.push("!");
+        if (pila.isEmpty()){
+            System.out.println("Esta vacía.");
+        }else {
+            System.out.println("No esta vacía.");
+        }
+        System.out.println(pila.size());
+        System.out.println(pila.peek());
+        System.out.println(pila.pop());
+    }
+
+    public static void Ex2(){
+
+        int num = 3;
+        Stack<Integer> pila = new Stack<>();
+
+        for (int i = 0; i  < num ; i++){
+            System.out.println("Introduce el número " + (i+1) + " :");
+            pila.push(entry.nextInt());
+        }
+
+        System.out.println("-------------");
+
+        while (!pila.isEmpty()){
+            System.out.println(pila.pop());
+        }
+
+    }
+
+    public static void Ex3(){
+
+        System.out.println("Introduce la operación:");
+
+        if (esBalanceado(entry.next())){
+
+            System.out.println("Formato correcto.");
+
+        }else {
+
+            System.out.println("Formato incorrecto.");
+
+        }
+    
+    }
+
+    public static boolean esBalanceado(String chain){
+
+        Stack<String> pila = new Stack<>();
+        String[] chainValue = chain.split("");
+
+        for ( int i = 0 ; i<chain.length() ; i++ ){
+
+            if (chainValue[i].equals("(")){
+
+                pila.push(chainValue[i]);
+
+            } else if (chainValue[i].equals(")") && pila.contains("(")) {
+                pila.pop();
+            }
+
+        }
+
+        return pila.isEmpty();
+
+    }
+}
--- PFuncional/src/main/java/org/example/Main.java
+++ PFuncional/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Stack<Integer> pila = new Stack<>();
+        pila.push(5);
+        pila.push(10);
+        pila.push(2);
+        pila.push(7);
+        pila.push(22);
+        pila.push(34);
+
+        System.out.println(pila.peek());
+
+        for (Integer num : pila){
+            System.out.println(num);
+        }
+
+        pila.pop();
+        System.out.println(pila.peek());
+
+        while (!pila.isEmpty()){
+            pila.pop();
+        }
     }
 }
